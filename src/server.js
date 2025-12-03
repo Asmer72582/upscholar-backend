a165b3b02ee27ac58588b381569bd78d21d12774
Asmer72582 <asmer72582@gmail.com>
2025-12-03 21:14:19 +0530

    change

--- src/server.js
+++ src/server.js
@@ -106,7 +106,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Start server
 const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
 
